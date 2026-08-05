0cd626c02ef05d2ff0f5bcae9ca44cfcc22d5f07
obour1 <obourone.media@gmail.com>
2026-08-05 21:28:38 +0300

    Rename supabase client to obourSupabase

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 const SUPABASE_URL = "https://zygltpoglafcalepdgjq.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_0Ypzu8StOaoPshJL6R0vgQ_bsJItrBM";
-
-const supabase = window.supabase.createClient(
+window.obourSupabase = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
+
+

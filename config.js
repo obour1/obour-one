8c785b46c7c3b0ca67b9b0b3c9d97b71df123eb5
obour1 <obourone.media@gmail.com>
2026-08-05 21:13:26 +0300

    Initialize Supabase client in config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://zygltpoglafcalepdgjq.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_0Ypzu8StOaoPshJL6R0vgQ_bsJItrBM";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
